4d3bba267439915c669a182b46f632e21a3792f7
chanzz0 <chanzz1@foxmail.com>
2024-11-28 11:41:31 +0800

    update

--- src/git.sh
+++ src/git.sh
@@ -1,15 +1,18 @@
+########################### 初始化操作 ###########################
 # 初始化一个 git 仓库
 git init
 # 克隆仓库
 git clone <url> [name]
 
+# 添加远程仓库（关联远程仓库的地址）
+git remote add <remote-name> <url>
 
-# 添加文件到缓存区
-git add
-# 提交（后面双引号内填写提交信息）。提交信息一般要写明此次提交的详情
+
+# 添加文件到缓存区（一般都提交所有文件，需要忽略处理的操作配合.gitignore来实现）
+git add .
+# 提交到本地仓库（引号内填写提交信息）。提交信息一般要写明此次提交的详情
 git commit -m "<message>"
-# 查看当前所在的分支（列出本地的所有分支）
-git branch
+
 # 创建分支
 git branch <name>
 # 切换到某分支
@@ -20,8 +23,8 @@ git branch -d <name>
 # 强制删除某分支
 git branch -D <name>
 
-# 合并分支
-git merge
+# 合并分支（将指定分支合并到当前分支）
+git merge <branch-name>
 # 推送到远程仓库
 git push
 # 推送指定分支到远程仓库（后面填写的分支与当前所在分支无关）
@@ -44,25 +47,32 @@ git reset <filename>
 git reset --hard <commit ID>
 
 
-
+########################### 检查操作 ###########################
 # 查看文件状态
 git status
 # 查看提交历史记录
 git log
 
-
-# 查看远程仓库
-# git remote
-git remote -v
-# 添加远程仓库
-git remote add <remote-name> <url>
-
-
+# 查看当前所在的分支（列出本地的所有分支）
+git branch
 # 列出远程的所有分支
 git branch --remote
 git branch -r
 # 列出本地和远程的所有分支
 git branch --all
 git branch -a
+
+# 查看远程仓库
+# git remote
+git remote -v
+
+
+########################### 标签操作 ###########################
+# 列出所有标签
+git tag
+# 创建标签
+git tag <tag-name>
+# 将标签推送到远程仓库
+git push origin <tag-name>
 
 
