39d11ecd58d12eef45a6f0ad44c8df6e73d69387
Aaron Chan <cs.yelling123@passinbox.com>
2025-04-22 17:08:13 +0800

    update

--- script/common.sh
+++ script/common.sh
@@ -1,9 +1,14 @@
+# - Only push
+# - First pull, then push
 
 # Add all files to git workdir
 git add .
 
 # Commit via default editor
 git commit
+
+# If it must be pull
+git pull
 
 # Push to remote repo
 git push
